d286c60da18c09b89bb24a662d4e6cb00177c1bc
Mike-Novoselov <siberia.krs.novoselov@gmail.com>
2020-07-18 21:59:44 +0700

    bull sheat

---
+++ st_lesson.py
@@ -0,0 +1,15 @@
+box1 = 15
+box2 = 25
+box3 = box1 + box2
+print(box3)
+
+a = 20
+b = 30
+c = a + b
+print(c)
+
+name = input('Введите ваше имя:')
+age = input("введите солько вам лет:")
+Name = "Вас зовут: " + name
+age = "Вам " + age + "лет"
+print(name); print(age)
