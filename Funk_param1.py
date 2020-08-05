ed57434dea461d90919d28dd85d05c29bbc6b473
Mike-Novoselov <siberia.krs.novoselov@gmail.com>
2020-08-05 22:36:05 +0700

    Funktion

---
+++ Funk_param1.py
@@ -0,0 +1,14 @@
+a= int(input())
+b= int(input())
+while a !=100 and b !=100:
+
+def her (a, b): # функция принимает аргументы "а и б"
+# Тело функции
+    if a>b:
+        print(a, "максимально")
+    elif a==b:
+        print(b, "равно", b)
+    else:
+        print(b, "максимально")
+
+her(a, b)
